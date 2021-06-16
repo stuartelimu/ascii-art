a741419b07450f1a921b88e95d7dd3c7e50bdfd0
stuartelimu <stuartelimu@gmail.com>
2021-06-16 07:15:31 +0300

    print pixel data

--- script.py
+++ script.py
@@ -1,7 +1,22 @@
 from PIL import Image
 
 # read image
-im = Image.open('img.png')
+im = Image.open('ascii-pineapple.jpg')
 
 print('Successfully loaded image!')
 print(f'Image size: {im.size[0]} x {im.size[1]}')
+
+# load pixel data of image
+px = im.load()
+
+# 2D array of pixels
+pixel_list = [[x] for x in list(im.getdata())]
+
+# loop through array and print all pixels of image
+print('Iterating through pixel contents')
+for x in range(len(pixel_list)):
+    for y in range(len(pixel_list[x])):
+        pixel = pixel_list[x][y]
+        print(pixel)
+
+# print(pixel_list)
