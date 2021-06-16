179a4fe3cf21e9dd8d683cbda314670083f3d9cf
stuartelimu <stuartelimu@gmail.com>
2021-06-16 10:50:52 +0300

    print ascii character

--- script.py
+++ script.py
@@ -9,8 +9,10 @@ print(f'Image size: {im.size[0]} x {im.size[1]}')
 # load pixel data of image
 px = im.load()
 
+width, height = im.size
+
 # 2D array of pixels
-pixel_list = [[x] for x in list(im.getdata())]
+pixel_list = [[x for x in list(im.getdata())]]
 
 # loop through array and print all pixels of image
 print('Iterating through pixel contents')
@@ -20,11 +22,34 @@ for x in range(len(pixel_list)):
         # print(pixel)
 
 # construct brightness matrix
-brightness_matrix = [[(x[0]+x[1]+x[2])//3] for x in list(im.getdata())]
+brightness_matrix = [[round((x[0]+x[1]+x[2])/3) for x in list(im.getdata())]]
 
 # loop through brightness matrix
 print('Iterating through pixel brightness')
 for x in range(len(brightness_matrix)):
     for y in range(len(brightness_matrix[x])):
         pixel = brightness_matrix[x][y]
-        print(pixel)
+        # print(pixel)
+
+chars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+print(len(list(chars)))
+
+value_of_char = round(255/len(chars))
+
+print(value_of_char)
+
+chars_matrix = []
+for x in range(len(brightness_matrix)):
+    for y in range(len(brightness_matrix[x])):
+        pixel = brightness_matrix[x][y]
+        # print(pixel)
+        char_index = round(pixel/value_of_char)
+        char_list = [chars[char_index]]
+        chars_matrix.append(char_list)
+        # print(chars[char_index])
+
+print('Iterating through pixel brightness')
+for x in range(len(chars_matrix)):
+    for y in range(len(chars_matrix[x])):
+        pixel = chars_matrix[x][y]
+        print(pixel*3, end='')
